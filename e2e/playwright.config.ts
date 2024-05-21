fd8c2de878d283c1e780d1a1acf1fe1858905e31
Deependra Patel <ideependra.patel@gmail.com>
2024-05-21 14:27:18 +0530

    playwright

---
+++ e2e/playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig ({
+    testDir: './src/test',
+    timeout: 30 * 1000,
+    expect: {
+        timeout: 10 * 1000,
+    },
+    fullyParallel: true,
+    forbidOnly: !!process.env['CI'],
+    retries: process.env['CI'] ? 2 : 0,
+    workers: process.env['CI'] ? 1 : undefined,
+    reporter: [['html', {outputFolder: './report/playwright-report'}]],
+    outputDir: './report/test-results',
+    use: {
+        actionTimeout: 0,
+        baseURL: 'https://playwright.dev/',
+        screenshot: 'on',
+        trace: 'on-first-retry',
+    },
+    projects: [
+        {
+            name: 'chrome',
+            use: {
+                ...devices['Desktop Chrome'],
+                channel: 'chrome',
+                ignoreHTTPSErrors: true,
+            }
+        }
+    ],
+})
